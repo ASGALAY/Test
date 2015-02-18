e796befbd0f119c6bff8bc03a82701c2eea4f8cf
Анатолий Галай <asgala@mail.ru>
2015-02-18 16:27:52 +0300

    Первый commit. Сделал 3 первых окна. Кода еще нет

---
+++ Mathematics 1.3/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Mathematics 1.3
+//
+//  Created by Анатолий Галай on 05.02.15.
+//  Copyright (c) 2015 Father & Son. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController
+{
+
+//****************************************************************
+//эти два метода были по умолчанию
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
